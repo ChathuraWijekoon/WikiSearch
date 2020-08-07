4f941da9ab89603e8c81ffa0760129cec40b1f74
Chathura B. Wijekoon <39031203+ChathuraWijekoon@users.noreply.github.com>
2020-08-08 01:56:28 +0530

    Add files via upload

---
+++ WikiSearch/WikiSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WikiSearch
+//
+//  Created by Chathura Wijekoon on 8/7/20.
+//  Copyright © 2020 Chathura Wijekoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
